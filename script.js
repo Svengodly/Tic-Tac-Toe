9653c2811db80f2be5d9d97ff0bdffad0503bf66
Rickey Morris <rmorris.rickey@gmail.com>
2024-07-17 18:36:39 -0400

    Added some notes to the Javascript file.

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 // TODO: Create Game, Board, and Player objects.
+
+// Game:
+    // Track the game's state - Who won/loss, Whose turn it is
+
+// Gameboard:
+    // Hold and keep record of spaces - If a space is occupied, then that space cannot be overidden. Responsible for drawing itself.
+
+// Player:
+    // Keeps details for each player - Each person has either an 'X' or 'O' marker. We can use Player 1 and Player 2 for the player names.
